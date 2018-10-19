47557a18c0b4bd104cccadcb40cc9717ee857bdd
David Weil <gamemx25@gmail.com>
2018-10-18 23:06:03 -0500

    Add Cross-Platform Build, HC-SR04 Arduino Build Setup

---
+++ Wall-e/HC-SR04/HC-SR04.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		HC_SR04.ino
+ Created:	10/18/2018 11:00:56 PM
+ Author:	Weil
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Wall-e/Wall-e/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+    printf("hello from Wall-e!\n");
+	printf("*Plants Tree*\n");
+    return 0;
+}
