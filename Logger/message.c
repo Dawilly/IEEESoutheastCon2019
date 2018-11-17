c589095c9ff21fed517579b09fad24dcc31a2d63
David Weil <gamemx25@gmail.com>
2018-11-17 01:01:15 -0600

    Add Timestamp, message type to message class

--- Logger/message.c
+++ Logger/message.c
@@ -2,13 +2,16 @@
 #include "message.h"
 
 typedef struct _message {
-	char* timestamp;
+	time_t timestamp;
 	char* value;
 	messageType type;
 } message;
 
+char* convertMessageType(messageType);
+
 message *newMessage(char* v, messageType type) {
 	message* msg = malloc(sizeof(message));
+	msg->timestamp = time(0);
 	msg->value = v;
 	msg->type = type;
 	return msg;
@@ -16,12 +19,29 @@ message *newMessage(char* v, messageType type) {
 
 void displayMessage(FILE* fp, void* value) {
 	message* msg = value;
-	fprintf(fp, "%s", msg->value);
+	char* readableTime = ctime(&msg->timestamp);
+	readableTime[strlen(readableTime) - 1] = '\0';
+	fprintf(fp, "[%s] [%s]: %s\n", readableTime, convertMessageType(msg->type), msg->value);
 }
 
 void freeMessage(message* msg) {
-	free(msg->timestamp);
 	free(msg->value);
 	free(msg);
 	return;
+}
+
+char* convertMessageType(messageType type) {
+	switch (type) {
+		case Trace:
+			return "Trace";
+		case Info:
+			return "Info";
+		case Warning:
+			return "Warning";
+		case Error:
+			return "Error";
+		case Fatal:
+			return "Fatal";
+	}
+	return "Unknown";
 }
--- Logger/message.h
+++ Logger/message.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 #ifndef __MESSAGE_INCLUDED__
 #define __MESSAGE_INCLUDED__
