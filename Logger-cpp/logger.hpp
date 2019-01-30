12fcc77004e3d5fe1b1c303836ca07e61519c3fa
David Weil <gamemx25@gmail.com>
2019-01-30 07:21:45 -0600

    Add Logger-cpp

--- Logger-cpp/logger.hpp
+++ Logger-cpp/logger.hpp
@@ -1,24 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include <queue>
 #include <thread>
 #include "message.hpp"
 
 #ifndef __LOGGER_CPP_INCLUDED__
 #define __LOGGER_CPP_INCLUDED__
 
-using namespace std;
-
 class Logger {
 	private:
-		vector<message> Messages;
-		ifstream &file;
+		std::queue<message> Messages;
+		std::ofstream file;
+		std::thread _thread;
+		std::string delimiter;
+		bool running;
 		
-		Bootstrapper();
-		WriteMessage(ifstream&, message);
+		static void Bootstrapper(Logger*);
+		void WriteMessage(std::ofstream&, message);
 	public:
-		Logger(string);
-		void AddMessage(string v, messageType type);
+		Logger(std::string, std::string);
+		void AddMessage(std::string*, messageType);
+		void AddMessage(int*, messageType);
+		void AddMessage(double*, messageType);
+		void AddMessage(void*, messageType, valueType);
 		void Run();
-}
+		void Stop();
+};
 
 #endif
