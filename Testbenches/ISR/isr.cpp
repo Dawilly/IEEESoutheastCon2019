3ddd0bd0a7b5e90745535d2ff10169de5f8c4777
tdwhalen <tdwhalen@crimson.ua.edu>
2019-03-11 23:47:45 -0500

    Pi ISR: Interrupts only from high to low?

--- Testbenches/ISR/isr.cpp
+++ Testbenches/ISR/isr.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <pigpio>
+#include <chrono>
+#include <thread>
+#include <pigpio.h>
 
-#define PIN 0
+#define PIN 4
 
 using namespace std;
 
 void handle_interrupt(int gpio, int level, uint32_t tick);
 
 int main(void) {
-    gpioInitialize();
+    gpioInitialise();
     gpioSetMode(PIN, PI_INPUT);
     gpioSetPullUpDown(PIN, PI_PUD_UP);
     gpioSetISRFunc(PIN, EITHER_EDGE, 0, handle_interrupt);
@@ -22,4 +24,5 @@ int main(void) {
 void handle_interrupt(int gpio, int level, uint32_t tick) {
     cout << "Pin " << gpio << " caused interrupt of level " << level
          << " at time " << tick << endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(15));
 }
