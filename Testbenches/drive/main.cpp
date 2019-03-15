2bd60f7dcffcb98bda953111b2076082a058150f
Trenton Whalen <tdwhalen@crimson.ua.edu>
2019-03-14 19:17:27 -0500

    Initial test to drive with interrupt indication

--- Testbenches/drive/main.cpp
+++ Testbenches/drive/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "Serial8N1.h"
 
+#define PIN 4
+
 using namespace std;
+
+void handle_interrupt(int gpio, int level, uint32_t tick, void *flag);
 
 int main(int argc, char **argv) {
     // Check number of arguments
@@ -10,12 +16,50 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    // Set up GPIO 4 as "interrupt" to indicate arduino is ready for a message
+    gpioInitialise();
+    gpioSetMode(PIN, PI_INPUT);
+    gpioSetPullUpDown(PIN, PI_PUD_UP);
+
+    // Pass function to handle interrupt at rising and falling edges
+    bool arduino_ready = true;
+    gpioSetISRFuncEx(PIN, EITHER_EDGE, 0, handle_interrupt, &arduino_ready);
+    
     // Open serial port
     Serial8N1 arduino(argv[1], 9600);
-    //cout << "Waiting for remote system . . . " << arduino.readToken() << endl
-         //<< endl;
     
-    // Drive straight for 36 inches
-    arduino.write(to_string(1) + ' ' + to_string(36));
+    // Initialize a list of commands
+    string commands[] = {
+        to_string(1) + ' ' + to_string(36.0),
+        to_string(1) + ' ' + to_string(72.0)};
+    int size = size(commands);
+
+    // Execute commands when the arduino is ready to receive them
+    int i = 0;
+    while (i < size) {
+        if (arduino_ready == true) {
+            // Wait so we can see all the commands aren't sent at once 
+            this_thread::sleep_for(chrono::seconds(1));
+            // Send the new command
+            arduino.write(commands[i]);
+            i++;
+            arduino_ready = false;
+        }
+    }
     return 0;
+}
+
+// Function to handle change in level of GPIO 4 
+void handle_interrupt(int gpio, int level, uint32_t tick, void *flag) {
+    // At interrupt rising edge, arduino is ready for a new message
+    if (level == 1) {
+        cout << "Arduino is ready to receive a command at time " << tick << "."
+             << endl;
+        *flag = true;
+    }
+    // At falling edge, arduino has begun processing a command
+    else {
+        cout << "Arduino has begun processing a command at time " << tick
+             << "." << endl;
+    }
 }
