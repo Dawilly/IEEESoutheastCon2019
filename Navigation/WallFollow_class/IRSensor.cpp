99eca9ad453b1a045a2c76031f1dfa505c4b4ee5
David Weil <gamemx25@gmail.com>
2019-03-27 17:55:20 -0500

    Refactor WallFollow algorithm to a class

---
+++ Navigation/WallFollow_class/IRSensor.cpp
@@ -0,0 +1,29 @@
+#include "IRSensor.h"
+
+using namespace std;
+		
+IRSensor::IRSensor(bool debug = false) {
+	this->sensor = new Adafruit_VL53L0X();
+	this->measurement = new VL53L0X_RangingMeasurementData_t();
+	this->debug = debug;
+	this->success = true;
+	
+	if (!sensor->begin()) {
+		delete sensor;
+		delete measurement;
+		this->success = false;
+	}
+}
+
+uint16_t IRSensor::readRange() {
+	sensor->rangingTest(measurement, debug);
+	return measurement->RangeMilliMeter;
+}
+
+uint16_t IRSensor::lastReading() {
+	return measurement->RangeMilliMeter;
+}
+
+int IRSensor::RangeStatus() {
+	return measurement->RangeStatus;
+}
