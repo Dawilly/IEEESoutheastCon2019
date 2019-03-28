fd6747c688ccc48cd4af17ccc46a15c6d2ddec1f
David Weil <gamemx25@gmail.com>
2019-03-28 15:52:29 -0500

    Restructured entire Repository

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include <cstdio>
 
-
-int main(int argc, char* argv[]) {
-	fwd_timed(
+int main()
+{
+    printf("hello from AL_e!\n");
+    return 0;
 }
+
+//#include <stdio.h>
+
+
+//int main(int argc, char* argv[]) {
+//	fwd_timed(
+//}
