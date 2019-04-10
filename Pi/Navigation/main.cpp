80e7a8c9edb4a014cf168a3729914cf5fbfae0a9
Trenton Whalen <tdwhalen@crimson.ua.edu>
2019-04-10 14:41:41 -0500

    New correct algorithm that uses encoder ticks

--- Pi/Navigation/main.cpp
+++ Pi/Navigation/main.cpp
@@ -12,19 +12,21 @@
 #include <pigpio.h>
 
 #define PIN 18
-inline double readingsToInches(double r1, double r2) {
-    return ((r1 + r2) / 25.4 / 2.0) + 4.5;
-}
 #define PI 3.14159265358979323846
 #define radiansToDegrees(theta) (theta * 180.0 / PI)
 
 using namespace std;
 
-vector<double> correct(vector<double> point, double heading, vector<double> readings);
+vector<double> correct(vector<double> point, double heading,
+        vector<double> readings, vector<double> deltas);
+double wallDistance(double expected, double r1, double r2);
 string makeTurnCommand(vector<double> point, Vertex *end, double *heading);
 string makeDriveCommand(vector<double> point, Vertex *end, double heading);
+//void sendDriveCommand(Serial8N1 *arduino, string command, vector<double> point,
+       //Vertex *end, double *heading, vector<double> *readings,
+       //vector<double> *deltas);
 void readArduinoData(Serial8N1 *arduino, double *heading,
-        vector<double> *readings);
+        vector<double> *readings, vector<double> *deltas);
 void handle_interrupt(int gpio, int level, uint32_t tick, void *flag);
 
 bool arduino_ready = true;
@@ -62,6 +64,7 @@ int main(int argc, char **argv) {
     // Initialize IMU heading and IR readings
     double heading;
     vector<double> readings(8, 0.0);
+    vector<double> deltas(2, 0.0);
 
     // Initialize position and begin iterating through waypoints
     Vertex *position = waypoints[0];
@@ -82,12 +85,12 @@ int main(int argc, char **argv) {
             end = path.front();
 
             // Update IMU heading and IR readings
-            readArduinoData(&arduino, &heading, &readings);
+            readArduinoData(&arduino, &heading, &readings, &deltas);
 
             // Initialize spatial point and correct with arduino data
             double tmp[] = {start->getX(), start->getY()};
             vector<double> point(tmp, tmp + sizeof(tmp) / sizeof(tmp[0]));
-            point = correct(point, heading, readings);
+            point = correct(point, heading, readings, deltas);
 
             // Calculate and perform a turn command
             string command = makeTurnCommand(point, end, &heading);
@@ -97,10 +100,10 @@ int main(int argc, char **argv) {
             while(arduino_ready != true);
 
             // Update IMU heading and IR readings
-            readArduinoData(&arduino, &heading, &readings);
+            readArduinoData(&arduino, &heading, &readings, &deltas);
 
             // Update spatial point with arduino data
-            point = correct(point, heading, readings);
+            point = correct(point, heading, readings, deltas);
 
             // Calculate and perform a drive command
             command = makeDriveCommand(point, end, heading);
@@ -123,48 +126,77 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-vector<double> correct(vector<double> point, double heading, vector<double> readings) {
+vector<double> correct(vector<double> point, double heading,
+        vector<double> readings, vector<double> deltas)
+{
+    // Add the expected deltas to our previous point
+    point[0] += deltas[0];
+    point[1] += deltas[1];
+
     // Direction is up
     if ((heading > 355.0 && heading < 360.0) ||
             (heading > 0.0 && heading < 5.0))
     {
         point[0] = (point[0] > 48.5) ?
-            (97.0 - readingsToInches(readings[0], readings[1]))
-            : (readingsToInches(readings[2], readings[3]));
-        //point[1] = (point[1] > 48.5) ?
-            //(97.0 - readingsToInches(readings[2], readings[3]))
-            //: (readingsToInches(readings[6], readings[7]));
+            (97.0 - wallDistance(97.0 - point[0], readings[0], readings[1]))
+            : (wallDistance(point[0], readings[2], readings[3]));
+        point[1] = (point[1] > 48.5) ?
+            (97.0 - wallDistance(97.0 - point[1], readings[4], readings[5]))
+            : (wallDistance(point[1], readings[6], readings[7]));
     }
     // Direction is down
     else if (heading > 175.0 && heading < 185.0) {
         point[0] = (point[0] > 48.5) ?
-            (97.0 - readingsToInches(readings[2], readings[3]))
-            : (readingsToInches(readings[0], readings[1]));
-        //point[1] = (point[1] > 48.5) ?
-            //(97.0 - readingsToInches(readings[6], readings[7]))
-            //: (readingsToInches(readings[2], readings[3]));
+            (97.0 - wallDistance(97.0 - point[0], readings[2], readings[3]))
+            : (wallDistance(point[0], readings[0], readings[1]));
+        point[1] = (point[1] > 48.5) ?
+            (97.0 - wallDistance(97.0 - point[1], readings[6], readings[7]))
+            : (wallDistance(point[1], readings[4], readings[5]));
     }
     // Direction is left
     else if (heading > 265.0 && heading < 275.0) {
-        //point[0] = (point[0] > 48.5) ?
-            //(97.0 - readingsToInches(readings[6], readings[7]))
-            //: (readingsToInches(readings[2], readings[3]));
+        point[0] = (point[0] > 48.5) ?
+            (97.0 - wallDistance(97.0 - point[0], readings[6], readings[7]))
+            : (wallDistance(97.0 - point[0], readings[4], readings[5]));
         point[1] = (point[1] > 48.5) ?
-            (97.0 - readingsToInches(readings[0], readings[1]))
-            : (readingsToInches(readings[2], readings[3]));
+            (97.0 - wallDistance(97.0 - point[1], readings[0], readings[1]))
+            : (wallDistance(point[1], readings[2], readings[3]));
     }
     // Direction is right
     else if (heading > 85.0 && heading < 95.0) {
-        //x = (point[0] > 48.5) ?
-            //(97.0 - readingsToInches(readings[2], readings[3]))
-            //: (readingsToInches(readings[6], readings[7]));
+        point[0] = (point[0] > 48.5) ?
+            (97.0 - wallDistance(97.0 - point[0], readings[4], readings[5]))
+            : (wallDistance(point[0], readings[6], readings[7]));
         point[1] = (point[1] > 48.5) ?
-            (97.0 - readingsToInches(readings[2], readings[3]))
-            : (readingsToInches(readings[0], readings[1]));
+            (97.0 - wallDistance(97.0 - point[1], readings[2], readings[3]))
+            : (wallDistance(point[1], readings[0], readings[1]));
     }
-    // Otherwise, just use original point values (do nothing)
+    // Otherwise, just return the point with the added deltas
 
     return point;
+}
+
+// Returns the distance to the wall. Accounts for invalid IR
+//  measurements and unexpected objects seen by the IRs
+double wallDistance(double expected, double r1, double r2) {
+    // Determine the measured distance using valid values only
+    double irDistance;
+    if (r1 != -1.0 && r2 != -1.0) {
+        irDistance = (r1 + r2) / 2.0;
+    }
+    else if (r1 == -1.0) {
+        irDistance = r2;
+    }
+    else if (r2 == -1.0) {
+        irDistance = r1;
+    }
+    else {
+        irDistance = expected;
+    }
+
+    // Return the wall distance measured by the IRs if it is about what is
+    //  expected. Otherwise, an unexpected object was seen.
+    return (abs(irDistance - expected) > 12.0) ? expected : irDistance;
 }
 
 string makeTurnCommand(vector<double> point, Vertex *end, double *heading) {
@@ -225,17 +257,19 @@ string makeDriveCommand(vector<double> point, Vertex *end, double heading) {
 }
 
 void readArduinoData(Serial8N1 *arduino, double *heading,
-        vector<double> *readings)
+        vector<double> *readings, vector<double> *deltas)
 {
     // Send command for arduino to send necessary data
     arduino->write("7 0.0");
     arduino_ready = false;
     while(arduino_ready != true);
 
-    // Read received data and update IMU heading and IR readings
+    // Read received data and update IMU heading, IR readings, and delta values
     (*heading) = arduino->readReal();
     for (vector<double>::iterator r = readings->begin(); r != readings->end();
             r++) (*r) = arduino->readReal();
+    (*delta)[0] = arduino->readReal();
+    (*delta)[1] = arduino->readReal();
 }
 
 // Function to handle change in level of GPIO 18
